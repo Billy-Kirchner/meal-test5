37d8c6bbef52571b8abb41b6b5a1c7f7945dd93c
Billy Kirchner <bkirchner27@gmail.com>
2019-01-24 16:54:57 -0600

    Added remove feature for ingredients.

--- src/main/java/org/launchcode/mealplanner/controllers/IngredientController.java
+++ src/main/java/org/launchcode/mealplanner/controllers/IngredientController.java
@@ -60,4 +60,14 @@ public class IngredientController {
 
         return "ingredient/view";
     }
+
+    @RequestMapping(value = "remove/{id}", method = RequestMethod.GET)
+    public String removeIngredient( Model model, @PathVariable(value ="id") int id) {
+
+        model.addAttribute("ingredient", ingredientDao.findById(id).orElse(null));
+
+        ingredientDao.deleteById(id);
+
+        return "ingredient/remove";
+    }
 }
