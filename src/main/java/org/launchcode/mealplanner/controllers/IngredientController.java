73375cc5ab6105be9533c9bd4acbe4a544a2825b
Billy Kirchner <bkirchner27@gmail.com>
2019-01-24 00:19:18 -0600

    Added index and add pages for Ingredient

--- src/main/java/org/launchcode/mealplanner/controllers/IngredientController.java
+++ src/main/java/org/launchcode/mealplanner/controllers/IngredientController.java
@@ -1,17 +1,54 @@
 package org.launchcode.mealplanner.controllers;
 
 
+import org.launchcode.mealplanner.models.Ingredient;
+import org.launchcode.mealplanner.models.data.IngredientDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
 
 @Controller
 @RequestMapping("ingredient")
 public class IngredientController {
 
-    @RequestMapping(value ="")
-    @ResponseBody
-    public String index () {
-        return "Ingredients go here!";
+    @Autowired
+    private IngredientDao ingredientDao;
+
+    @RequestMapping(value = "")
+    public String index( Model model) {
+
+        model.addAttribute("ingredients", ingredientDao.findAll());
+        model.addAttribute("title", "Available Ingredients");
+
+        return "ingredient/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddIngredientForm(Model model) {
+        model.addAttribute("title", "Add New Ingredient");
+        model.addAttribute(new Ingredient());
+
+        return "ingredient/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddIngredientForm(@ModelAttribute @Valid Ingredient newIngredient, Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add New Ingredient");
+            return "ingredient/add";
+        }
+
+        ingredientDao.save(newIngredient);
+        newIngredient.calculateTotalFat();
+        newIngredient.calculateNetCarbohydrate();
+        return "redirect:";
     }
 }
---
+++ src/main/java/org/launchcode/mealplanner/models/Ingredient.java
@@ -0,0 +1,200 @@
+package org.launchcode.mealplanner.models;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+
+
+import javax.persistence.GeneratedValue;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min=3, max=20, message = "Name must be between 3 and 20 characters")
+    private String name;
+    @NotNull
+    private double calories;
+    @NotNull
+    private double saturatedFat;
+    @NotNull
+    private double polyUnsaturatedFat;
+    @NotNull
+    private double monoUnsaturatedFat;
+    @NotNull
+    private double transFat;
+    private double totalFat;
+    @NotNull
+    private double cholesterol;
+    @NotNull
+    private double sodium;
+    @NotNull
+    private double potassium;
+    @NotNull
+    private double totalCarbohydrate;
+    @NotNull
+    private double dietaryFiber;
+    @NotNull
+    private double sugar;
+    private double netCarbohydrate;
+    @NotNull
+    private double protein;
+
+
+
+    public Ingredient (String name, double calories, double saturatedFat, double polyUnsaturatedFat, double monoUnsaturatedFat, double transFat,
+                       double cholesterol, double sodium, double potassium, double totalCarbohydrate, double dietaryFiber, double sugar, double protein) {
+        this.name =  name;
+        this.calories = calories;
+        this.saturatedFat = saturatedFat;
+        this.polyUnsaturatedFat = polyUnsaturatedFat;
+        this.monoUnsaturatedFat = monoUnsaturatedFat;
+        this.transFat = transFat;
+        this.cholesterol = cholesterol;
+        this.sodium = sodium;
+        this.potassium = potassium;
+        this.totalCarbohydrate = totalCarbohydrate;
+        this. dietaryFiber = dietaryFiber;
+        this. sugar = sugar;
+        this.protein = protein;
+    }
+
+    public Ingredient () { }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCalories() {
+        return calories;
+    }
+
+    public void setCalories(double calories) {
+        this.calories = calories;
+    }
+
+    public double getSaturatedFat() {
+        return saturatedFat;
+    }
+
+    public void setSaturatedFat(double saturatedFat) {
+        this.saturatedFat = saturatedFat;
+    }
+
+    public double getPolyUnsaturatedFat() {
+        return polyUnsaturatedFat;
+    }
+
+    public void setPolyUnsaturatedFat(double polyUnsaturatedFat) {
+        this.polyUnsaturatedFat = polyUnsaturatedFat;
+    }
+
+    public double getMonoUnsaturatedFat() {
+        return monoUnsaturatedFat;
+    }
+
+    public void setMonoUnsaturatedFat(double monoUnsaturatedFat) {
+        this.monoUnsaturatedFat = monoUnsaturatedFat;
+    }
+
+    public double getTransFat() {
+        return transFat;
+    }
+
+    public void setTransFat(double transFat) {
+        this.transFat = transFat;
+    }
+
+    public double getCholesterol() {
+        return cholesterol;
+    }
+
+    public void setCholesterol(double cholesterol) {
+        this.cholesterol = cholesterol;
+    }
+
+    public double getSodium() {
+        return sodium;
+    }
+
+    public void setSodium(double sodium) {
+        this.sodium = sodium;
+    }
+
+    public double getPotassium() {
+        return potassium;
+    }
+
+    public void setPotassium(double potassium) {
+        this.potassium = potassium;
+    }
+
+    public double getTotalCarbohydrate() {
+        return totalCarbohydrate;
+    }
+
+    public void setTotalCarbohydrate(double totalCarbohydrate) {
+        this.totalCarbohydrate = totalCarbohydrate;
+    }
+
+    public double getDietaryFiber() {
+        return dietaryFiber;
+    }
+
+    public void setDietaryFiber(double dietaryFiber) {
+        this.dietaryFiber = dietaryFiber;
+    }
+
+    public double getSugar() {
+        return sugar;
+    }
+
+    public void setSugar(double sugar) {
+        this.sugar = sugar;
+    }
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public void setProtein(double protein) {
+        this.protein = protein;
+    }
+
+    public double getTotalFat() {
+        return totalFat;
+    }
+
+    public double getNetCarbohydrate() {
+        return netCarbohydrate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void calculateTotalFat () {
+        totalFat = saturatedFat+polyUnsaturatedFat+monoUnsaturatedFat+transFat;
+    }
+
+    public void calculateNetCarbohydrate () {
+
+       if ((totalCarbohydrate - dietaryFiber - sugar) < 0) {
+           netCarbohydrate = 0;
+       }
+       else {
+           netCarbohydrate = totalCarbohydrate - dietaryFiber - sugar;
+       }
+
+    }
+
+}
